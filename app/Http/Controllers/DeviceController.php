0c0a8dc8a48c5174e408833a4ae5c5d9ba9bab94
Naveen Karia <naveen.karia29@gmail.com>
2024-01-22 22:54:14 +0500

    Put method API

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -25,4 +25,18 @@ class DeviceController extends Controller
             return ["Result" => "Operation failed"];
         }
     }
+
+    function update(Request $req)
+    {
+        $device = Device::find($req->id);
+        $device->name = $req->name;
+        $device->member_id = $req->member_id;
+        $result = $device->save();
+        if ($result) {
+            return ["result" => "Data has been updated"];
+        } else {
+            return ["result" => "Update operation has been failed"];
+        }
+
+    }
 }
